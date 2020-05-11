97ccebf1f5c4f4f08fa7d9c9758f839331eb3ab7
631203498@qq.com <631203498@qq.com>
2020-05-11 20:09:14 +0800

    二叉树

---
+++ BinaryTreeProject/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree("ABCDE#F");
+            binaryTree.PreOrder(binaryTree.Head);       
+            Console.WriteLine();
+            binaryTree.MidOrder(binaryTree.Head);
+            Console.WriteLine();
+            binaryTree.AfterOrder(binaryTree.Head);
+            Console.WriteLine();
+            Console.Read();
+        }    
+    }
+
+    public class TreeNode
+    {
+        public object Data { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(object data)
+        {
+            Data = data;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public TreeNode Head { get; set; }
+        public string ConstructStr { get; set; }
+
+        public BinaryTree(string str)
+        {
+            ConstructStr = str;
+            Head = new TreeNode(ConstructStr[0]);
+            Add(Head,0);
+        }
+
+        private void Add(TreeNode parent,int index)
+        {
+            int leftIndex = 2 * index + 1;
+            if (leftIndex<ConstructStr.Length)
+            {
+                if (ConstructStr[leftIndex]!='#')
+                {
+                    parent.Left = new TreeNode(ConstructStr[leftIndex]);
+                    Add(parent.Left,leftIndex);
+                }
+            }
+            int rightIndex = 2 * index + 2;
+            if (rightIndex<ConstructStr.Length)
+            {
+                if (ConstructStr[rightIndex] != '#')
+                {
+                    parent.Right = new TreeNode(ConstructStr[rightIndex]);
+                    Add(parent.Right,rightIndex);
+                }
+            }
+        }
+
+        public void PreOrder(TreeNode node)
+        {
+            if (node!=null)
+            {
+                Console.Write(node.Data);
+                PreOrder(node.Left);
+                PreOrder(node.Right);
+            }
+        }
+
+        public void MidOrder(TreeNode node)
+        {
+            if (node != null)
+            {              
+                MidOrder(node.Left);
+                Console.Write(node.Data);
+                MidOrder(node.Right);
+            }
+        }
+
+        public void AfterOrder(TreeNode node)
+        {
+            if (node != null)
+            {
+                AfterOrder(node.Left);             
+                AfterOrder(node.Right);
+                Console.Write(node.Data);
+            }
+        }
+    }
+}
